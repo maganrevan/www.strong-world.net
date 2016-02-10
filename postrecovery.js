5b41c50ee19dd4e42f022571505e08dde635c8f8
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2016-02-10 08:27:09 +0100

    include the js-sourcecode

--- postrecovery.js
+++ postrecovery.js
@@ -3,5 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$(document).ready(function () {
+    setTimeout(function () {
+        /*Beginn Postsicherung*/
+        if ($('.sceditor-container').find('textarea').length !== 0) {
+            if (localStorage.getItem("post") !== '' && localStorage.getItem("post") !== null) {
+                var bConfirm = confirm("Möchten Sie den letzten Post wieder laden?");
+            }
 
+            $('.mainoption, .liteoption').bind('click', function () {
+                $.fctResetPost("post");
+            });
+
+            if (bConfirm === true) {
+                $('.sceditor-container').children('textarea').val(localStorage.getItem('post').replace(/<br[^>]*>/g, "\n"));
+                $.fctResetPost("post");
+            } else {
+                $.fctResetPost("post");
+            }
+
+            $('.sceditor-container').children('textarea').bind('keyup', function () {
+                localStorage.setItem("post", $(this).val().replace(/\n/g, '<br/>'));
+            });
+        }
+        /*Ende Postsicherung*/
+
+        $.fctResetPost = function (name) {
+            localStorage.setItem(name, '');
+        };
+    }, 1500);
+});
 
